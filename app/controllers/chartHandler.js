a6c17e9d83951f226c9a1d0419fd9b83cf4fbf2d
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-12 12:51:50 +0000

    chart object added

---
+++ app/controllers/chartHandler.js
@@ -0,0 +1,40 @@
+'use strict'; 
+ 
+module.exports = { 
+    
+    makeChart: function(){
+        var chart = {  
+            rangeSelector: {
+                selected: 4
+            },
+
+            yAxis: {
+                labels: {
+                    formatter: function () {
+                        return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                    }
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 2,
+                    color: 'silver'
+                }]
+            },
+
+            plotOptions: {
+                series: {
+                    compare: 'percent',
+                    showInNavigator: true
+                }
+            },
+
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                valueDecimals: 2,
+                split: true
+            },
+
+            series: seriesOptions
+        }
+    }
+}
