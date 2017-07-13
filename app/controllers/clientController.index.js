e0518709dedb3862069a1595e99147d62d052769
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-13 11:27:46 +0000

    added create chart function

--- app/controllers/clientController.index.js
+++ app/controllers/clientController.index.js
@@ -4,6 +4,8 @@
     
     var stockCode = document.getElementById("stock-code");
     var addStock = document.getElementById("add-stock");
+    var remove = document.querySelector(".remove");
+    var parsedData;
     
     function createChart(chartData){
         Highcharts.stockChart('chart', chartData);
@@ -13,10 +15,19 @@
        addStock.addEventListener('click', function(){
            console.log("hi");
            ajaxFunctions.ajaxPostRequest({dataset_code: stockCode.value}, appUrl + '/stock', function(data){
-               var parsedData = JSON.parse(data);
-               console.log(parsedData);
+                if(data){
+                    parsedData = JSON.parse(data);
+                    console.log(parsedData.series);
+                    createChart(parsedData);
+                }
            }); 
         });
     });
+    
+    remove.addEventListener('click', function(e){
+        ajaxFunctions.ajaxRequest('GET', appUrl + '/stock', function(data){
+            console.log(data);
+        })
+    })
     
 })();
