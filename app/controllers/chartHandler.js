edf9bbec09609670752848879a1495eae79e3fb6
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-13 11:27:19 +0000

    added series for chart and findMyStock method

--- app/controllers/chartHandler.js
+++ app/controllers/chartHandler.js
@@ -6,18 +6,26 @@ var request = require('request');
 module.exports = { 
     
     addStock : function(dataset_code){
+  //      console.log(dataset_code);
         var newStock = new Stock();
         return new Promise(function(resolve, reject){
-            request(process.env.API_URL + '/WIKI/' + dataset_code + '?column_index=1', function (error, response, body) {
+            request(process.env.API_URL + '/WIKI/' + dataset_code + '.json?column_index=1', function (error, response, body) {
+          //      console.log(body);
                 if(error) console.error(error);
                 else if(response.statusCode !== 200) console.log(response.status);
                 else{
                     var data = JSON.parse(body);
+                  //  console.log(data);
                     newStock.dataset_code = data.dataset.dataset_code;
                     newStock.database_code = data.dataset.database_code;
-                    newStock.name = data.dataset.name;
-                    newStock.data = data.dataset.data;
-                    console.log(newStock);
+                    newStock.name = data.dataset.name.split(')')[0] + ')';
+                    newStock.data = data.dataset.data.reverse().map(info => {
+                        return [
+                            (new Date(info[0])).getTime(),
+                                info[1]
+                            ];
+                    });
+                  //  console.log(newStock);
                     newStock.save(function (err) {
 					    if (err) {
 						    throw err;
@@ -29,55 +37,96 @@ module.exports = {
         });
     },
     
-    findAllStocks: function(){
+    findMyStock: function(body){
         return new Promise(function(resolve, reject){
-           Stock.find({}, {name: 1, _id: 0},function(err, docs){
+           Stock.find({}, {dataset_code: 1, _id: 0},function(err, docs){
                if(err) console.error(err);
                console.log(docs);
-               resolve(docs);
+              // console.log(body);
+                if(docs.length === 0){
+                    resolve();
+                    console.log("empty");
+                }
+                else{
+               docs.every(function(doc, index){
+                  // console.log(body.dataset_code);
+                   if(doc.dataset_code === body.dataset_code){
+                       console.log("found");
+                       reject();
+                       return false;
+                   }
+                   else if(index === docs.length -1){
+                       console.log("not found");
+                       resolve();
+                   }
+                   else{
+                       return true;
+                   }
+               });
+                }
            }); 
         });
     },
     
-    deleteStock: function(id){
-      Stock.findByIdAndRemove(id, function(err, result){
-          if(err) console.error(err);
-      });
+    allStocks: function(){
+        return new Promise(function(resolve, reject){
+            Stock.find({}, {name: 1, data: 1, _id: 0}, function(err, docs){
+               // console.log(docs);
+                if(err) console.error(err);
+                resolve(docs);
+            });
+        });
     },
     
-    makeChart: function(){
+    deleteStock: function(id){
+     /* Stock.findByIdAndRemove(id, function(err, result){
+          if(err) console.error(err);
+      });*/
+      Stock.remove({}, function(err) { 
+   console.log('collection removed') 
+});
+    },
+    
+    makeChart: function(docs){
+        console.log(docs[0].data[0][0]);
         var chart = {  
             rangeSelector: {
-                selected: 4
-            },
+            selected: 4
+        },
 
-            yAxis: {
-                labels: {
-                    formatter: function () {
-                        return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                    }
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 2,
-                    color: 'silver'
-                }]
-            },
-
-            plotOptions: {
-                series: {
-                    compare: 'percent',
-                    showInNavigator: true
+        yAxis: {
+            labels: {
+                formatter: function () {
+                    return (this.value > 0 ? ' + ' : '') + this.value + '%';
                 }
             },
+            plotLines: [{
+                value: 0,
+                width: 2,
+                color: 'silver'
+            }]
+        },
 
-            tooltip: {
-                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-                valueDecimals: 2,
-                split: true
-            },
+        plotOptions: {
+            series: {
+                compare: 'value',
+                showInNavigator: true
+            }
+        },
 
-            series: seriesOptions
+        tooltip: {
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+            valueDecimals: 2,
+            split: true
+        },
+
+        series: docs.slice(0),
+        
+        credits: {
+            enabled: false
         }
+    };
+       // console.log(chart.series)
+        return chart;
     }
-}
+};
